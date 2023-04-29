433ef36ebdbf550a2957d5727ceec8d050b45f2e
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-04-29 18:15:26 +0200

    Game (3)

--- apps/game/main.c
+++ apps/game/main.c
@@ -6,11 +6,24 @@ Window window = {
 	.Title = "Test",
 	.OnKey = NULL};
 
-Colider colider = {
+Colider coliderGoal = {
 	.x = 0,
 	.y = 0,
 	.w = 20,
 	.h = 20};
+
+Colider coliderKill = {
+	.x = 20,
+	.y = 0,
+	.w = 1,
+	.h = 100};
+
+Colider coliderPlayer = {
+	.x = 0,
+	.y = 0,
+	.w = 10,
+	.h = 10,
+};
 
 void clear()
 {
@@ -31,16 +44,35 @@ void sleep(i32 mills)
 	return;
 }
 
-void renderCollider(Colider col)
+void renderCollider(Colider col, Color color)
 {
-	gfx_rect(col.x, col.y, col.w, col.h, COLOR_GREEN);
+	gfx_rect(col.x, col.y, col.w, col.h, color);
 }
 
-bool colliding(Colider col, i32 x, i32 y)
+bool inColider(Colider col, i32 x, i32 y)
 {
 	i32 x2 = col.x + col.w;
 	i32 y2 = col.y + col.h;
 	return (col.x <= x) && (x <= x2) && (col.y <= y) && (y <= y2);
+}
+
+bool overlap(Colider c1, Colider c2)
+{
+	// The rectangles don't overlap if
+	// one rectangle's minimum in some dimension
+	// is greater than the other's maximum in
+	// that dimension.
+	i32 c1x2 = c1.x + c1.w;
+	i32 c1y2 = c1.y + c1.h;
+	i32 c2x2 = c2.x + c2.w;
+	i32 c2y2 = c2.y + c2.h;
+
+	bool noOverlap = c1.x > c2x2 ||
+					 c2.x > c1x2 ||
+					 c1.y > c2y2 ||
+					 c2.y > c1y2;
+
+	return !noOverlap;
 }
 
 i32 main(void)
@@ -57,40 +89,56 @@ i32 main(void)
 	i32 player_y = y_move;
 
 	bool won = false;
+	bool alive = true;
 
 	clear();
 
 	loop
 	{
-		if (keyboard_is_key_pressed(KEY_W))
+
+		coliderPlayer.x = player_x;
+		coliderPlayer.y = player_y;
+
+		if (overlap(coliderKill, coliderPlayer))
 		{
-			player_y -= 10;
-		}
-		if (keyboard_is_key_pressed(KEY_S))
-		{
-			player_y += 10;
-		}
-		if (keyboard_is_key_pressed(KEY_A))
-		{
-			player_x -= 10;
-		}
-		if (keyboard_is_key_pressed(KEY_D))
-		{
-			player_x += 10;
-		}
-		clear();
-		renderCollider(colider);
-		if (colliding(colider, player_x, player_y) || won)
-		{
-			renderPlayer(player_x, player_y, COLOR_BLUE);
-			won = true;
-			font_string(50, 100, "YOU WON", ubuntu_bold, COLOR_RED, COLOR_BLACK);
+			font_string(50, 100, "YOU DEAD", ubuntu_bold, COLOR_RED, COLOR_BLACK);
 		}
 		else
 		{
-			renderPlayer(player_x, player_y, COLOR_RED);
+
+			if (keyboard_is_key_pressed(KEY_W))
+			{
+				player_y -= 10;
+			}
+			if (keyboard_is_key_pressed(KEY_S))
+			{
+				player_y += 10;
+			}
+			if (keyboard_is_key_pressed(KEY_A))
+			{
+				player_x -= 10;
+			}
+			if (keyboard_is_key_pressed(KEY_D))
+			{
+				player_x += 10;
+			}
+			clear();
+			renderCollider(coliderGoal, COLOR_GREEN);
+			renderCollider(coliderKill, COLOR_RED);
+
+			if (overlap(coliderGoal, coliderPlayer) || won)
+			{
+				renderPlayer(player_x, player_y, COLOR_BLUE);
+				won = true;
+				font_string(50, 100, "YOU WON", ubuntu_bold, COLOR_RED, COLOR_BLACK);
+			}
+			else
+			{
+				renderPlayer(player_x, player_y, COLOR_WHITE);
+			}
+
+			sleep(100);
 		}
-		sleep(100);
 	}
 
 	return 0;
