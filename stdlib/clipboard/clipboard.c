a267c8ad134e1352cf02a4d8098e4ba8e3be2bcc
Tim Gabrikowski <tim@gabrikowski.de>
2023-05-14 14:40:10 +0200

    document clipboard

--- stdlib/clipboard/clipboard.c
+++ stdlib/clipboard/clipboard.c
@@ -1,3 +1,10 @@
+/**
+ * @file    clipboard.c
+ * @author  Tim Gabrikowski, Anton Tchekov
+ * @version 0.1
+ * @date    14.05.2023
+ */
+
 #include <clipboard.h>
 #include <string.h>
 
--- stdlib/clipboard/clipboard.h
+++ stdlib/clipboard/clipboard.h
@@ -1,3 +1,11 @@
+/**
+ * @file    clipboard.h
+ * @author  Tim Gabrikowski, Anton Tchekov
+ * @version 0.1
+ * @date    14.05.2023
+ * @brief   Implement a OS-wide Clipboard
+ */
+
 #ifndef __CLIPBOARD_H__
 #define __CLIPBOARD_H__
 
@@ -7,14 +15,29 @@ typedef enum {
     CLIPBOARD_TYPE_TEXT
 } ClipboardType;
 
+/**
+ * @brief   Save Text to clipboard
+*/
 void clipboard_save_text(const char *text);
 
+/**
+ * @brief   Save Text to the clipboard when the length is known
+*/
 void clipboard_save_text_len(const char *text, i32 len);
 
+/**
+ * @brief   Get Data from Clipboard
+*/
 const void *clipboard_get(void);
 
+/**
+ * @brief   Get the length of the Data in Clipboard
+*/
 i32 clipboard_len(void);
 
+/**
+ * @brief   Get the stored Datatype from the clipboard
+*/
 ClipboardType clipboard_type(void);
 
 #endif /* __CLIPBOARD_H__ */
