6d9a567c5b63c6d6e36e25dbf7ebcfc72d7ebac9
Tim Gabrikowski <tim@gabrikowski.de>
2023-05-27 14:11:35 +0200

    rust

---
+++ apps/rust-test/target/riscv32imac-unknown-none-elf/debug/deps/rust_test-e77c1a69385e5973.d
@@ -0,0 +1,5 @@
+/home/tim/projects/osdev/apps/rust-test/target/riscv32imac-unknown-none-elf/debug/deps/rust_test-e77c1a69385e5973: src/main.rs
+
+/home/tim/projects/osdev/apps/rust-test/target/riscv32imac-unknown-none-elf/debug/deps/rust_test-e77c1a69385e5973.d: src/main.rs
+
+src/main.rs:
---
+++ apps/rust-test/target/riscv32imac-unknown-none-elf/debug/rust-test.d
@@ -0,0 +1,1 @@
+/home/tim/projects/osdev/apps/rust-test/target/riscv32imac-unknown-none-elf/debug/rust-test: /home/tim/projects/osdev/apps/rust-test/src/main.rs
