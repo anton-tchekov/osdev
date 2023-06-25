77e01bfc6bcc6dd8aac33d2dab1147071455e875
Tim Gabrikowski <timgabrikowski@outlook.de>
2023-06-25 13:28:35 +0200

    str_contains and SearchBar Programms filter function

--- platform/x86/src/gui/searchbar.c
+++ platform/x86/src/gui/searchbar.c
@@ -2,17 +2,19 @@
 
 #define SEARCH_HEIGHT 40
 
-/* Define Struct for a SearchResult (only text by now but add icon later?) */
-typedef struct SearchResult
-{
-	char *title;
-} SearchResult;
+char* searchTerm = "";
 
 ColorTheme nordPalette = {
 	.primary = 0xFF40342E,
 	.secondary = 0xFF52423B,
 	.background = 0xFF5E4C43,
 	.text = 0xFFF4EFEC};
+
+#define PROGRAM_COUNT 4
+/* Create Array of Programms */
+Program programs[PROGRAM_COUNT] = {
+	{"Firefox"}, {"VScode"}, {"Files"}, {"Tetris"}
+};
 
 void searchbar_render(Framebuffer *fb)
 {
@@ -23,21 +25,34 @@ void searchbar_render(Framebuffer *fb)
 	framebuffer_rect(fb, 0, SEARCH_HEIGHT, fb->Width, 5, nordPalette.primary);
 
 	/* Search title */
-	font_string(fb, 15, 10, "Type to search...", font_noto, nordPalette.text);
+	if(searchTerm == "") {
+		font_string(fb, 15, 10, "Type to search...", font_noto, nordPalette.text);
+	} else {
+		font_string(fb, 15, 10, searchTerm, font_noto, nordPalette.text);
+	}
 
-	/* Create Array of SearchResults */
-	SearchResult results[4] = {
-		{"Firefox"}, {"VScode"}, {"Files"}, {"Tetris"}
-	};
+	/* Create Results array and count value */
+	Program results[PROGRAM_COUNT] = {};
+	i32 resultsCount = 0;
+
+	for (i32 i = 0; i < PROGRAM_COUNT; i++)
+	{
+		/* check for every element if it contains the search term */
+		if(str_contains(programs[i].title, searchTerm)) {
+			/* Add it to the results array and increment the Counter */
+			results[resultsCount] = programs[i];
+			resultsCount++;
+		}
+	}
 	/* Render all the results to list */
-	for (i32 i = 0; i < 4; i++)
+	for (i32 i = 0; i < resultsCount; i++)
 	{
 		render_searchresult(fb, results[i], 15, SEARCH_HEIGHT * (i+1));
 	}
 	
 }
 
-void render_searchresult(Framebuffer *fb, SearchResult res, i32 x, i32 y) {
+static void render_searchresult(Framebuffer *fb, Program res, i32 x, i32 y) {
 
 	/* "Icon" */
 	framebuffer_circle(fb, x + 10, y + (SEARCH_HEIGHT / 2) + 2, 5, nordPalette.text);
